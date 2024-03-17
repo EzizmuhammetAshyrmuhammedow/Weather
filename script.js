fe5e6ce5b7461227069e06c5f5c744d75c3ac4e6
Ezizmuhammet <emuhammedow1@gmail.com>
2024-03-17 08:46:00 -0400

    a

--- script.js
+++ script.js
@@ -16,6 +16,18 @@ async function checkWeather() {
   if (data.weather[0].main == "Clouds") {
     weatherIcon.src = "weather-app-img/images/clouds.png";
   }
+  else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "weather-app-img/images/clear.png";
+  }
+  else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "weather-app-img/images/rain.png";
+  }
+  else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "weather-app-img/images/mist.png";
+  }
+  else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "weather-app-img/images/drizzle.png";
+  }
 }
 searchBtn.addEventListener("click", () => {
   checkWeather();
